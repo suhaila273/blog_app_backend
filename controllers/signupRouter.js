52f54fe8328ffbbeb1b275b5234ab205be738890
suhaila273 <suhailasalam273@gmail.com>
2024-02-09 14:05:09 +0530

    new

--- controllers/signupRouter.js
+++ controllers/signupRouter.js
@@ -15,7 +15,7 @@ router.post("/add",async(req,res)=>{
    
     let {data}={"data":req.body}
     let password=req.body.password
-    hashPasswordGenerator(password).then(
+    /*hashPasswordGenerator(password).then(
         (hashedPassword)=>{
             console.log(hashedPassword)
             data.password=hashedPassword
@@ -27,7 +27,44 @@ router.post("/add",async(req,res)=>{
     )
         }
 
-    )
+    )*/
+    const hashedPassword=await hashPasswordGenerator(password)
+    data.password=hashedPassword
+    let signup=new signupModel(data)
+    let result=await signup.save()
+    res.json({
+        status:"success"
+    })
+})
+
+router.post("/login",async(req,res)=>{
+    let input=req.body
+    let email=req.body.email
+    let data=await signupModel.findOne({"email":email})
+    if(!data){
+        return res.json(
+            {
+                status:"invalid email id"
+            }
+        )
+    }
+    console.log(data)
+    let dbPassword=data.password
+    let inputPassword=req.body.password
+    console.log(dbPassword)
+    console.log(inputPassword)
+    const match=await bcrypt.compare(inputPassword,dbPassword)
+    if(!match)
+    {
+        return res.json(
+            {
+                status : "incorrect password"
+            }
+        )
+    }
+    res.json({
+        status : "success"
+    })
 })
 
 
