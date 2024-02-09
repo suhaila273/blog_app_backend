186423558624d189cd86cd8037098f7659d717f2
suhaila273 <suhailasalam273@gmail.com>
2024-02-09 15:17:22 +0530

    updated

--- controllers/postRouter.js
+++ controllers/postRouter.js
@@ -13,4 +13,15 @@ router.post("/addpost",async(req,res)=>{
         })
 })
 
+//TO get the userid as its name we have to populate the collections
+//in populate we will specify which content we need from the signup collection eg:name
+//-_id so that it wont display
+
+router.get("/viewall",async(req,res)=>{
+    let result=await postModel.find()
+    .populate("userId","name mobile address  -_id" )
+    .exec()
+    res.json(result)
+})
+
 module.exports=router
