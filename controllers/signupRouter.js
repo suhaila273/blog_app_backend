94488637266157699edb2e0a782f70fccb771404
suhaila273 <suhailasalam273@gmail.com>
2024-02-09 11:32:09 +0530

    security added

--- controllers/signupRouter.js
+++ controllers/signupRouter.js
@@ -1,16 +1,32 @@
 const express = require("express")
 
 const router=express.Router()
-
-
 const signupModel =require("../models/signupModel")
 
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
 router.post("/add",async(req,res)=>{
-    let data=req.body
-    let signup=new signupModel(data)
-    let result =await signup.save()
+   // let data=req.body
+   
+    let {data}={"data":req.body}
+    let password=req.body.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.password=hashedPassword
+            console.log(data)
+            let signup=new signupModel(data)
+    let result = signup.save()
     res.json(
         {status:"success"}
+    )
+        }
+
     )
 })
 
